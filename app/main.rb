b1909026cd63f150f4c5fbfd8773eab52953bd4c
Sato-Kunihiko <kunihiko.k.r.r@gmail.com>
2017-04-06 16:26:24 +0900

    complete

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,6 @@
 def main(argv)
   argv.each do |v|
-    puts v
+    result = (1 / Math.sqrt(5) * ( ((1 + Math.sqrt(5)) / 2) ** v.to_f - ((1 - Math.sqrt(5)) / 2) ** v.to_f )).to_i
+    print(result)
   end
 end
